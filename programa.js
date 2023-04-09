e2b8d41567282d524e0e2b9844eb683afde2ec98
Stephanie <stephanie.scm@gmail.com>
2023-04-09 09:22:55 -0500

    FRONTEND_CALCULADORA API

---
+++ programa.js
@@ -0,0 +1,12 @@
+const miBoton= document.getElementById("sumit");
+
+miBoton.addEventListener(
+    'click',
+    (event)=>{
+        event.preventDefault();
+        
+        const numero1=document.getElementById("numero1").value;
+        const numero2=document.getElementById("numero2").value;
+
+    }
+)
